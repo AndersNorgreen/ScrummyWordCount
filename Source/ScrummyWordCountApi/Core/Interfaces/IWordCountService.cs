06bbf3128bdf89d6701e3b0501ccde933968d4c8
Momo Amir Kruse Amer <maea@kollab.dk>
2026-05-07 13:03:01 +0200

    refactors method accessibility and changes test approach to focus on public method

--- Source/ScrummyWordCountApi/Core/Interfaces/IWordCountService.cs
+++ Source/ScrummyWordCountApi/Core/Interfaces/IWordCountService.cs
@@ -2,7 +2,5 @@ namespace ScrummyWordCountApi.Core.Interfaces;
 
 public interface IWordCountService
 {
-    Task<int> CountWordAsync(string url, string word, IEnumerable<string> pageWords);
-    Task SaveSearchAsync(string url, string word, int count);
     Task<int> CountAndSaveAsync(string url, string word, IEnumerable<string> pageWords);
 }
