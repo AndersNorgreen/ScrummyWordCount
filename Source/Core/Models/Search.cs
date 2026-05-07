84a162d542b4b65a53d4f5b1db0108c07d77936a
Jamie <jamiedelasencerie@gmail.com>
2026-05-07 11:35:01 +0200

    Scaffolds database through EF core

---
+++ Source/Core/Models/Search.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScrummyWordCountApi.Core.Models;
+
+public partial class Search
+{
+    public int Id { get; set; }
+
+    public DateTime Searchedat { get; set; }
+
+    public string Url { get; set; } = null!;
+
+    public string Searchquery { get; set; } = null!;
+
+    public int Numberofoccurrences { get; set; }
+}
---
+++ Source/Infrastructure/ScrummyWordCountContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ScrummyWordCountApi.Core.Models;
+
+namespace ScrummyWordCountApi.Infrastructure;
+
+public partial class ScrummyWordCountContext : DbContext
+{
+    public ScrummyWordCountContext(DbContextOptions<ScrummyWordCountContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Search> Searches { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Search>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("searches_pkey");
+
+            entity.ToTable("searches");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Numberofoccurrences).HasColumnName("numberofoccurrences");
+            entity.Property(e => e.Searchedat)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("searchedat");
+            entity.Property(e => e.Searchquery)
+                .HasMaxLength(255)
+                .HasColumnName("searchquery");
+            entity.Property(e => e.Url)
+                .HasMaxLength(255)
+                .HasColumnName("url");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
